890b507f6a90f2d8d47a41d421cae96f19c36ab3
jld3103 <jld3103yt@gmail.com>
2018-06-26 16:37:30 +0200

    Added grades to AG plan

--- ags.js
+++ ags.js
@@ -41,9 +41,10 @@ this.readAGList = () => {
         const g = t[j];
         const s = g.split('\n');
         const o = {
-          name: s[0].trim(),
+          name: s[0].replace(/Kl\. [0-9]/g, '').trim(),
           time: '',
-          room: ''
+          room: '',
+          grades: ''
         };
         for (let k = 0; k < s.length; k++) {
           if (s[k].toLowerCase().includes('uhr')) {
@@ -58,6 +59,20 @@ this.readAGList = () => {
             }
           } else if (s[k].toLowerCase().includes('raum') || s[k].toLowerCase().includes('r.') || s[k].toLowerCase().includes('comp.') || s[k].toLowerCase().includes('gr ha') || s[k].toLowerCase().includes('ghalle') || s[k].toLowerCase().includes('g halle') || s[k].toLowerCase().includes('aula') || s[k].toLowerCase().includes('beekstraße')) {
             o.room = s[k].toLowerCase().replace('raum ', '').replace('r.', '').replace('comp.', '').replace(/gr ha|ghalle|g halle/g, 'Große Halle').replace('musikraum', 'Musikraum').replace('aula', 'Aula').replace('beekstraße', 'Beekstraße').replace(/\./g, '').trim();
+          } else if (s[k].toLowerCase().includes('gst') || s[k].toLowerCase().includes('alle') || s[k].toLowerCase().includes('stufe') || s[k].toLowerCase().includes('kl.')) {
+            const grades = ['5', '6', '7', '8', '9', 'ef', 'q1', 'q2'];
+            let m = s[k].toLowerCase().replace('jgst.', '').replace('gst.', '').replace('ef', 'EF').replace('q1', 'Q1').replace('q2', 'Q2').replace('oberstufe', 'Oberstufe').replace('mittelstufe', 'Mittelstufe').replace('unterstufe', 'Unterstufe').replace('alle', 'Alle').replace(/[1-2]\.hj/g, '').replace(/ +(?= )/g, '').replace('/', '-').trim();
+            if (m.includes('kl.')) {
+              m = m.split('kl.')[1].trim();
+            }
+            if (m.includes('ab')) {
+              m = m.replace('ab', '').trim() + '-Q2';
+            }
+            if (m.includes('-')) {
+              o.grades = m.split('-')[0].trim() + ' - ' + m.split('-')[1].trim();
+            } else {
+              o.grades = m;
+            }
           }
         }
         ags[i].ags.push(o);
